730d537f0e8d4c4b859165b804ee6e7841131c1a
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2024-12-10 10:55:22 +0100

    initial commit

---
+++ MusicCatalog/src/test/java/com/MusicCatalog/MusicCatalog/MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MusicCatalog.MusicCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
